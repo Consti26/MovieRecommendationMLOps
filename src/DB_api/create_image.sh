a6114129c00b7fff77208ee28b1c142a0c62164d
Akramerhh <43610160+Akramerhh@users.noreply.github.com>
2025-01-27 11:01:23 +0100

    Update create_image.sh
    create_image.sh corrected

--- src/DB_api/create_image.sh
+++ src/DB_api/create_image.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+echo $SCRIPT_DIR
+# Define the relative paths based on the script's location
+PROCESSED_DATA_PATH=$(realpath "$SCRIPT_DIR/processed_data")
+RAW_DATA_PATH=$(realpath "$SCRIPT_DIR/raw_data")
+
+mkdir $PROCESSED_DATA_PATH
+mkdir $RAW_DATA_PATH
 
 docker image build . -t db_api:latest
