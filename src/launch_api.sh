76bff4b348d940da910d03bcf1b2a0cc1392331a
Ubuntu <ubuntu@ip-172-31-44-224.eu-west-1.compute.internal>
2025-02-08 12:33:25 +0000

    switched from conda to pip, to reduce image size

--- src/launch_api.sh
+++ src/launch_api.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 # Make sure the script directory is exported as an environment variable
+docker stop $(docker ps -a -q)
 export SCRIPT_DIR=$(dirname "$(realpath "$0")")
+sudo rm -rf ./models/mlflow/workdir/*
+sudo rm -rf ./models/mlflow/workdir/.*
+# sudo rm -rf ~/.local/share/Trash/*
+sudo rm -rf ./data/raw_data/*
 
 # Run Docker Compose
 docker system prune -f -a
-docker build prune -f
+docker builder prune -f
 docker image prune -f
 docker container prune -f
+# docker volume rm mlflow_volume
 docker volume prune -f
-docker-compose up --build -d #--no-cache
+# docker-compose up --build -d #--no-cache
+
+
+docker-compose up -d 
+docker system prune -f -a
+# sudo rm -rf /tmp/*.tmp
 
 # Base URL of the API
-# BASE_URL="http://localhost:8000"
-# echo "Testing Database Creation Endpoint (remove existing)"
-# curl -X POST "${BASE_URL}/api/v1/database/create?remove_existing=true"
-# echo -e "\n"
+BASE_URL="http://localhost:8000"
+echo "Testing Database Creation Endpoint (remove existing)"
+curl -X POST "${BASE_URL}/api/v1/database/create?remove_existing=true"
+echo -e "\n"
--- src/test_db_preprocess.sh
+++ src/test_db_preprocess.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 docker stop $(docker ps -a -q)
-docker rmi $(docker images -q) -f
-sudo rm -rf /home/ubuntu/oct24_cmlops_int_movie_reco/src/models/mlflow/workdir/*
+# docker rmi $(docker images -q) -f
+
+# docker image rm mlfow:latest
+# sudo rm -rf /home/ubuntu/oct24_cmlops_int_movie_reco/src/models/mlflow/workdir/*
+# sudo rm -rf /home/ubuntu/oct24_cmlops_int_movie_reco/src/data/raw_data/*
 
 
 docker network prune -f
 docker volume prune -f
-# docker image rm mlfow:latest
+
 # docker image rm api_train_content:latest
 docker system prune -f
 docker volume rm mlflow_volume
@@ -48,22 +51,22 @@ docker container run -d \
 #             PREPROCESSING              #
 ##########################################
 
-# if  docker images --format '{{.Repository}}:{{.Tag}}' | grep -w "api_preprocess_content:latest" > /dev/null
-# then
-#   echo "It exists. Skip the Preprocess build..."
-# else
-#   echo "Building new Preprocess image.."
-#   # Build Docker image
-#   docker build -t api_preprocess_content:latest ./features
-# fi
+if  docker images --format '{{.Repository}}:{{.Tag}}' | grep -w "api_preprocess_content:latest" > /dev/null
+then
+  echo "It exists. Skip the Preprocess build..."
+else
+  echo "Building new Preprocess image.."
+  # Build Docker image
+  docker build -t api_preprocess_content:latest ./features
+fi
 
-# docker container run -d \
-#  --name preprocess_container\
-#  --env-file "$SCRIPT_DIR/features/.env_git"\
-#  --network test_network \
-#  -p 9000:9000\
-#  --rm\
-#  api_preprocess_content:latest
+docker container run -d \
+ --name preprocess_container\
+ --env-file "$SCRIPT_DIR/features/.env_git"\
+ --network test_network \
+ -p 9000:9000\
+ --rm\
+ api_preprocess_content:latest
 ##########################################
 #             Create Volume              #
 ##########################################
@@ -79,45 +82,45 @@ docker volume create \
 #                 MLFOW                  #
 ##########################################
 
-# if  docker images --format '{{.Repository}}:{{.Tag}}' | grep -w "mlfow:latest" > /dev/null
-# then
-#   echo "It exists. Skip the MLFlow build..."
-# else
-#   echo "Building new MLFlow image.."
-#   # Build Docker image
-#   docker build -t mlfow:latest ./models/mlflow
-# fi
+if  docker images --format '{{.Repository}}:{{.Tag}}' | grep -w "mlfow:latest" > /dev/null
+then
+  echo "It exists. Skip the MLFlow build..."
+else
+  echo "Building new MLFlow image.."
+  # Build Docker image
+  docker build -t mlfow:latest ./models/mlflow
+fi
 
-# docker container run -d \
-#  --name mlflow_container\
-#  --env-file "$SCRIPT_DIR/models/mlflow/.env_git"\
-#  --mount source=mlflow_volume,target=/mlflow \
-#  --network test_network \
-#  -p 5000:5000\
-#  --rm\
-#  mlfow:latest
+docker container run -d \
+ --name mlflow_container\
+ --env-file "$SCRIPT_DIR/models/mlflow/.env_git"\
+ --mount source=mlflow_volume,target=/mlflow \
+ --network test_network \
+ -p 5000:5000\
+ --rm\
+ mlfow:latest
 
 ##########################################
 #             Train                      #
 ##########################################
 
-# if  docker images --format '{{.Repository}}:{{.Tag}}' | grep -w "api_train_content:latest" > /dev/null
-# then
-#   echo "It exists. Skip the Training build..."
-# else
-#   echo "Building new Training image.."
-#   # Build Docker image
-#   docker build -t api_train_content:latest ./models/training
-# fi
+if  docker images --format '{{.Repository}}:{{.Tag}}' | grep -w "api_train_content:latest" > /dev/null
+then
+  echo "It exists. Skip the Training build..."
+else
+  echo "Building new Training image.."
+  # Build Docker image
+  docker build -t api_train_content:latest ./models/training
+fi
 
-# docker container run \
-#  --name training_container\
-#  --env-file "$SCRIPT_DIR/models/training/.env_git"\
-#  --mount source=mlflow_volume,target=/mlflow \
-#  --network test_network \
-#  -p 8080:8080\
-#  --rm\
-#  api_train_content:latest
+docker container run \
+ --name training_container\
+ --env-file "$SCRIPT_DIR/models/training/.env_git"\
+ --mount source=mlflow_volume,target=/mlflow \
+ --network test_network \
+ -p 8080:8080\
+ --rm\
+ api_train_content:latest
 
 
 ##########################################
