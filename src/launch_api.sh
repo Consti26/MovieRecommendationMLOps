e2bf9adca91e33dfe796451eff7797cfc9be80ef
Akramerhh <43610160+Akramerhh@users.noreply.github.com>
2025-02-11 12:39:21 +0100

    correct path, set all folders

--- src/launch_api.sh
+++ src/launch_api.sh
@@ -3,29 +3,32 @@
 docker stop $(docker ps -a -q)
 export SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+mkdir -p "${SCRIPT_DIR}/models/mlflow/workdir"
+mkdir -p  "${SCRIPT_DIR}/data/raw_data"
+mkdir -p  "${SCRIPT_DIR}/data/processed_data"
 
 #############################################
 # If you run for the first time, uncomment! #
 #############################################
 
-# sudo rm -rf ./models/mlflow/workdir/*
-# sudo rm -rf ./models/mlflow/workdir/.*
-# sudo rm -rf ~/.local/share/Trash/*
-# sudo rm -rf ./data/raw_data/*
-# docker system prune -f -a
-# docker volume prune -f
-# docker-compose up --build -d #--no-cache
-# Base URL of the API
-# BASE_URL="http://localhost:8000"
-# echo "Testing Database Creation Endpoint (remove existing)"
-# curl -X POST "${BASE_URL}/api/v1/database/create?remove_existing=true"
-# echo -e "\n"
+sudo rm -rf ${SCRIPT_DIR}/models/mlflow/workdir/*
+sudo rm -rf ${SCRIPT_DIR}/models/mlflow/workdir/.*
+sudo rm -rf ~/.local/share/Trash/*
+sudo rm -rf ${SCRIPT_DIR}/data/raw_data/*
+docker system prune -f -a
+docker volume prune -f
+docker-compose up --build -d #--no-cache
+Base URL of the API
+BASE_URL="http://localhost:8000"
+echo "Testing Database Creation Endpoint (remove existing)"
+curl -X POST "${BASE_URL}/api/v1/database/create?remove_existing=true"
+echo -e "\n"
 
 #######################
 # Any subsequent Run! #
 #######################
 
-docker builder prune -f
-docker-compose up #-d 
-docker builder prune -f
+# docker builder prune -f
+# docker-compose up #-d 
+# docker builder prune -f
 
