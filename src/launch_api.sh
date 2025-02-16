2e6745c9115b556077ca7753ba773d1930359da7
Constance26 <84840707+Constance26@users.noreply.github.com>
2025-02-16 21:23:00 +0100

    update streamlit_app and try to make work everthing

--- src/launch_api.sh
+++ src/launch_api.sh
@@ -3,26 +3,26 @@
 
 export SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# # Load environment variables from .env file
-# if [ -f "${SCRIPT_DIR}/.env" ]; then
-#     export $(cat "${SCRIPT_DIR}/.env" | grep -v '#' | awk '/=/ {print $1}')
-# fi
+ # Load environment variables from .env file
+ if [ -f "${SCRIPT_DIR}/.env" ]; then
+     export $(cat "${SCRIPT_DIR}/.env" | grep -v '#' | awk '/=/ {print $1}')
+ fi
 
-# mkdir -p "${SCRIPT_DIR}/models/mlflow/workdir"
-# mkdir -p  "${SCRIPT_DIR}/data/raw_data"
-# mkdir -p  "${SCRIPT_DIR}/data/processed_data"
+ mkdir -p "${SCRIPT_DIR}/models/mlflow/workdir"
+ mkdir -p  "${SCRIPT_DIR}/data/raw_data"
+ mkdir -p  "${SCRIPT_DIR}/data/processed_data"
 
 # #############################################
 # # If you run for the first time, uncomment! #
 # #############################################
 
-# sudo rm -rf ${SCRIPT_DIR}/models/mlflow/workdir/*
-# sudo rm -rf ${SCRIPT_DIR}/models/mlflow/workdir/.[^.]*
-# sudo rm -rf ~/.local/share/Trash/*
-# sudo rm -rf ${SCRIPT_DIR}/data/raw_data/*
-# sudo rm -rf ${SCRIPT_DIR}/data/processed_data/*
+ sudo rm -rf ${SCRIPT_DIR}/models/mlflow/workdir/*
+ sudo rm -rf ${SCRIPT_DIR}/models/mlflow/workdir/.[^.]*
+ sudo rm -rf ~/.local/share/Trash/*
+ sudo rm -rf ${SCRIPT_DIR}/data/raw_data/*
+ sudo rm -rf ${SCRIPT_DIR}/data/processed_data/*
 
-# echo ${SCRIPT_DIR}
+ echo ${SCRIPT_DIR}
 
 # # ###########################
 # # #   clean-up Docker State #
@@ -39,7 +39,7 @@ export SCRIPT_DIR=$(dirname "$(realpath "$0")")
 # docker volume rm $(docker volume ls -q)
 
 # # Prune the system to remove unused data
-# docker system prune -f
+ docker system prune -f
 
 # # Restart Docker service
 # if [ "$(uname)" == "Darwin" ]; then
@@ -58,18 +58,19 @@ export SCRIPT_DIR=$(dirname "$(realpath "$0")")
 # ##################
 
 # docker-compose up --build #--no-cache
-
+ docker compose up --build 
+sleep 10
 # Base URL of the API (using the environment variable)
-# BASE_URL="http://localhost:${DATABASE_PORT}"
-# echo "Testing Database Creation Endpoint (remove existing)"
-# curl -X POST "${BASE_URL}/api/v1/database/create?remove_existing=true"
-# echo -e "\n"
+ BASE_URL="http://localhost:${DATABASE_PORT}"
+ echo "Testing Database Creation Endpoint (remove existing)"
+ curl -X POST "${BASE_URL}/api/v1/database/create?remove_existing=true"
+ echo -e "\n"
 
 #######################
 # Any subsequent Run! #
 #######################
 
 # docker builder prune -f
-docker network create movie_recommendation_network
-docker-compose up --build #-d 
+#docker network create movie_recommendation_network
+#docker-compose up --build #-d 
 # docker builder prune -f
